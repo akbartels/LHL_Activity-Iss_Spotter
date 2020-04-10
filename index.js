7c4e7724b022fb92a052719f146c0613e0326113
Amy Bartels <akunrau@gmail.com>
2020-04-10 02:35:42 +0000

    created function fetchISSFlyOverTimes and tested it in index.js

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned IP:' , ip);
-// });
+  console.log('It worked! Returned IP:' , ip);
+});
 
-// fetchCoordsByIP('70.71.171.79', (error, data) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchCoordsByIP('70.71.171.79', (error, data) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
     
-//   console.log('It worked! Returned location:' , data);
-// });
+  console.log('It worked! Returned location:' , data);
+});
+
+fetchISSFlyOverTimes({ latitude: '49.21670', longitude: '-122.91670' }, (error, times) => {
+  
+  if (error) {
+    console.log("It didn't work!", error);
+  }
+  console.log("It worked! Returned times: ", times);
+});
--- iss.js
+++ iss.js
@@ -35,9 +35,28 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    const flybys = JSON.parse(body);
+    console.log(typeof flybys, flybys.response);
+  });
+};
+
+
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP};
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
+};
 
 
 
